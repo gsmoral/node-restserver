3d1346e35ce539522bb1b34382c1f93eb387d291
Guillermo <gsmoral@hotmail.com>
2020-03-23 17:41:35 +0100

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:h8c1CeXRmDN0CpV9@cluster0-ov3jm.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
