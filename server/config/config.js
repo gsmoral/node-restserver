5c1459f794a9e540bf8938548ea781672e6101ab
Guillermo <gsmoral@hotmail.com>
2020-03-23 16:55:12 +0100

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 //  Puerto
 // ====================
 process.env.PORT = process.env.PORT || 3000;
+
+// ====================
+//  Entorno
+// ====================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ====================
+//  Base de datos
+// ====================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://strider:h8c1CeXRmDN0CpV9@cluster0-ov3jm.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,37 +13,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-});
-app.post('/usuario', function(req, res) {
+// Importamos y usamos las rutas de usuario
+app.use(require('./routes/usuario'));
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
+mongoose.connect(process.env.URLDB, {
+    useCreateIndex: true,
+    //autoIndex: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json(`put usuario con id: ${id}`)
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json({
-        id
-    });
-});
+//var db = mongoose.connection;
+//db.on('error', console.error.bind(console, 'connection error:'));
+// mongoose.connect(environment.urlDB, {
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true
+// }, (err) => {
+// });
+// mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+//     if (err) throw err;
+//     console.log('Base de datos ONLINE');
+// });
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando peticiones en el puerto ${process.env.PORT}`);
